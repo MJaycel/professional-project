57669e944319d379ffc7cd36318b39a28f27ecfc
Jaycel Benedicto <benedicto.jaycel@gmail.com>
2022-02-18 00:47:04 +0000

    Events are displayed when the date is clicked

--- src/pages/Calendar.vue
+++ src/pages/Calendar.vue
@@ -1,20 +1,44 @@
 <template>
-    <div id="cal">
-        <div style="margin-top: 50px">
+    <div>
+        <div style="padding-top: 50px;background: #F8F2D1;" >
             <h2> Calendar</h2>
             <router-link :to="{name: 'home', params: userId}">Home</router-link>
         </div> 
+        <div class="row">
+            <div class="col-10">
+                <calendar-view
+                    :show-date="showDate"
+                    :items= items
+                    :show-times= true
+                    :time-format-options="{ hour: 'numeric', minute: '2-digit' }"
+                    @click-date="clickdate"
+                    class="theme-default holiday-us-traditional holiday-us-official cal">
+                    <calendar-view-header
+                        slot="header"
+                        slot-scope="t"
+                        :header-props="t.headerProps"
+                        @input="setShowDate" />
+                </calendar-view>                     
+            </div>
 
-        <calendar-view
-            :show-date="showDate"
-            :items= items
-            class="theme-default holiday-us-traditional holiday-us-official col-10">
-            <calendar-view-header
-                slot="header"
-                slot-scope="t"
-                :header-props="t.headerProps"
-                @input="setShowDate" />
-        </calendar-view>            
+            <div class="col-2 pt-3">
+                <p v-if="!this.dateClicked">{{this.today}}</p>
+                <h3>{{this.clickedDate}}</h3>
+
+                <div class="pt-3" v-for="item in itemsFound" :key="item._id">
+                    <div style="background: yellow">
+                        <h6>{{item.title}}</h6>                    
+                        <p>{{item.description}}</p> 
+                        <p>{{item.startTime}}</p>                        
+
+                    </div>
+
+
+                </div>
+
+
+            </div> 
+        </div>
     </div>
 </template>
 
@@ -34,9 +58,13 @@ export default {
         return{
             userId: localStorage.userId,
             showDate: new Date(),
-            
             //user events
-            items: []
+            items: [],
+            dateClicked: false,
+            clickedDate: '',
+            dateItems: [],
+
+            itemsFound: []
         }
     },
     components: {
@@ -45,11 +73,28 @@ export default {
     },
     mounted() {
         this.getAllEvents()
+
+        if(this.dateClicked === false){
+            this.today = new Date()
+            this.today = this.today.toDateString().slice(0,-4).trim();
+            console.log(this.today);
+        }
+
     },
     methods: {
-
         setShowDate(d) {
             this.showDate = d;
+        },
+        clickdate(d){
+            this.dateClicked = true
+            
+            this.date = new Date(d)
+            this.clickedDate = this.date.toDateString().slice(0,-4).trim();
+            console.log(this.clickedDate);    
+            
+            this.getEventsInDate()
+
+
         },
         //////get all events
         getAllEvents() {
@@ -59,6 +104,26 @@ export default {
                     console.log('EVENTS', response.data)
                 })
                 .catch(error => console.log(error))     
+        },
+        //////get events in date clicked
+        getEventsInDate() {
+            //get all events first
+            axios.get(`http://localhost:3030/calendar/${this.$route.params.id}`)
+            .then(response=> {
+                this.dateItems = response.data
+                    console.log('this is date items',this.dateItems)
+                    console.log('this is the dates ',this.dateItems[0].startDate)
+
+                    this.dateItems.forEach(events => {
+                        this.dates = new Date(events.startDate)
+                        events.startDate = this.dates.toDateString().slice(0,-4).trim()
+
+                        this.itemsFound = this.dateItems.filter(events => events.startDate === this.clickedDate)
+                        console.log('startDates',events.startDate)
+                    });
+                    console.log(this.itemsFound)
+            })
+            .catch(error => console.log(error))    
         }
     },
 
@@ -66,12 +131,12 @@ export default {
 </script>
 
 <style>
-    #cal {
+.cal{
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         color: #2c3e50;
         height: 85vh;
-        width: 90vw;
-        margin-left: 1.5vw;
+        /* width: 90vw; */
+        margin-left: vw;
         /* margin-right: auto;  */
     }
 </style>
