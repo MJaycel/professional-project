0b9272ff3f7060df77964e0e9534a6fffdd5f77f
Jaycel Benedicto <benedicto.jaycel@gmail.com>
2022-03-05 01:16:26 +0000

    fixed error with draw circle

--- src/components/CircularProgress.js
+++ src/components/CircularProgress.js
@@ -11,7 +11,8 @@ export function setPercentage(_percentage){
 }
 
 //link showing how to use p5 in vue
-//https://medium.com/js-dojo/experiment-with-p5-js-on-vue-7ebc05030d33
+//https://medium.com/js-dojo/experiment-with-p5-js-on-vue-7ebc05030d33 **
+
 
 export function main(_p5){
     p5 = _p5;
@@ -22,22 +23,61 @@ export function main(_p5){
     }
 
     p5.draw = () =>{
-        p5.clear();
+        // p5.clear();
+        p5.background(255);
         p5.translate(p5.width/2, p5.height/2); // change center
+        p5.drawCircle('#efefef', 360);
+        p5.drawCircle('#000', percentage);
         
-         p5.drawCircle('#efefef', 360);
-         p5.drawCircle('#000', percentage);
+        // let startPercentage = 0;
+        // var arcInterval = setInterval(function() {
+        //     startPercentage += 1;
+        //     // p5.clear();
+        //     p5.drawCircle('#000', startPercentage);
+        //     if(startPercentage >= percentage) clearInterval(arcInterval);
+        // }, 1000);
         
         //text - percentage
         p5.drawText();
     }
 
-    p5.drawCircle = (color,percent) => {
+    // p5.drawCircle = () => {
+    //     let startPercentage = 0;
+
+    //     // let fps = 1000/200;
+    //     var arcInterval = setInterval(function() {
+    //         startPercentage += 2;
+    //         // p5.clear();
+    //         // p5.beginShape();
+    //         // p5.strokeWeight(15);
+    //         // // p5.stroke('#df00');
+    //         // p5.strokeStyle = '#b1b1b1';
+    //         // p5.noFill();
+    //         // //arc(x, y, width, height, start, stop);
+    //         // p5.arc(0, 0, radius, radius, (Math.PI/180) * 270, (Math.PI/180) * (270 + 360), false );
+    //         // p5.lineWidth = lineWidth;
+
+    //         p5.beginShape();
+    //         p5.strokeWeight(15);
+    //         p5.stroke('#efefef');
+    //         // p5.strokeStyle = '#efefef';
+    //         p5.noFill();
+    //         //arc(x, y, width, height, start, stop);
+    //         p5.arc(0, 0, radius, radius, (Math.PI/180) * 270, (Math.PI/180) * (270 + startPercentage), false );
+    //         p5.lineWidth = lineWidth;
+
+    //         if(startPercentage >= percentage) clearInterval(arcInterval);
+
+        
+    //     }, 10);
+    // }
+
+    p5.drawCircle = (color, percent) => {
         p5.beginShape();
         p5.strokeWeight(15);
         p5.stroke(color);
         p5.noFill();
-        //arc(x,y,width,height,start, stop);
+        //arc(x, y, width, height, start, stop);
         p5.arc(0, 0, radius, radius, (Math.PI/180) * 270, (Math.PI/180) * (270 + percent), false );
         p5.lineWidth = lineWidth;
     }
@@ -48,6 +88,7 @@ export function main(_p5){
         p5.strokeWeight(2)
         p5.textSize(27);
         p5.fill(0);
+
         /* 
            Change degrees percentage to 100%
            e.g 120 degrees (2 tasks done) 
@@ -57,7 +98,6 @@ export function main(_p5){
 
         //text(word, x, y);
         p5.text(Math.floor((percentage/360)*100) + '%', -30, 0);
-        // p5.endShape();
     }
 }
 
